74d61aa8d1e61e324ab46cfaa5820c0ab822a11f
chutsun2 <tsungyo.chu@mail.utoronto.ca>
2024-05-30 10:44:47 +0800

    (retrieved studio link information)

--- animerec.py
+++ animerec.py
@@ -1,1 +1,14 @@
+import requests 
+from bs4 import BeautifulSoup
+
+URL = "https://myanimelist.net/anime.php"
+r = requests.get(URL).text
+
+soup = BeautifulSoup(r,'lxml')
+page = soup.find_all('div', class_ = "genre-link")
+studios = page[-3].find_all('div',class_ = "genre-list al")
+for studio in studios:
+    print(studio.a.prettify())
+
+
 
